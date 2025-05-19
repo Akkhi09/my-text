8e492be80e6840c48d3e4e8328106d6afd1d61b0
Akkhi09 <akkhiprodigo23@gmail.com>
2025-05-19 17:43:12 +0200

    adicionei mais dois arquivos

---
+++ index.js
@@ -0,0 +1,9 @@
+const V = 158.5;
+function conversor(x)
+{
+    return x * V;
+}
+console.log("O valor convertido em reais é: " + conversor(5));
+let x = parseInt("5");
+console.log(x);
+console.log((+"2.4") + (+"3"));
